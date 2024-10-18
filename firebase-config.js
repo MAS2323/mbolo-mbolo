13bc27b97589a2f70894d79638aacfa7f8c74ee4
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2024-10-18 19:24:03 +0800

    agaregando firebase

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAu3FCgLVSbqCxcU7WV1bRAlHIyxKyIN3E",
+  authDomain: "auth-mbolo.firebaseapp.com",
+  projectId: "auth-mbolo",
+  storageBucket: "auth-mbolo.appspot.com",
+  messagingSenderId: "366983463414",
+  appId: "1:366983463414:web:b3ba57b946f60bd162c7d7",
+};
